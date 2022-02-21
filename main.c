8a2af8466f004669945e24511e2761c51372a8c2
Lemuna60 <98328620+Lemuna60@users.noreply.github.com>
2022-02-21 15:18:06 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+county  Library management system.
+Lemuna junior
+Feb,2022
+GNU GCC
+C99
+MIT
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\tCounty Library\n");
+    printf("Welcome Evans.\n");
+    printf("What would you like to do?\n");
+    printf("1. Add new user\n");
+    printf("2.Add Book.\n");
+    printf("selected action: ");
+    return 0;
+}
